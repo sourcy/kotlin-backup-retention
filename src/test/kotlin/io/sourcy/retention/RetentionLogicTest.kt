8684c48874a891a745370f518cade23f894d817f
Daniel Selinger <d.selinger@sourcy.io>
2018-02-04 13:16:54 +0100

    logic tests

--- src/test/kotlin/io/sourcy/retention/RetentionLogicTest.kt
+++ src/test/kotlin/io/sourcy/retention/RetentionLogicTest.kt
@@ -1,5 +1,6 @@
 package io.sourcy.retention
 
+import org.assertj.core.api.AbstractObjectAssert
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import java.io.File
@@ -9,30 +10,95 @@ class RetentionLogicTest : AbstractBaseTest() {
     private val retentionLogic = RetentionLogic(dryRunArgumentsAnd(emptyArray()), settings)
 
     @Test
-    fun testKeepTodaysDaily() {
+    fun testKeepCurrentDaily() {
         val result = retentionLogic.calculateRetentionInfo(File("some_2018-01-18_05-07-59.tar.gz"))
-        when (result) {
-            is RetentionInfo.Info -> assertThat(result)
-                    .matches { it.isDaily && !it.isExpired }
-        }
+        assertThatRetentionInfo(result)
+                .matches { it.isDaily && !it.isExpired }
     }
 
     @Test
-    fun testKeepSevenDailys() {
+    fun testKeepSevenDailies() {
         val result = retentionLogic.calculateRetentionInfo(File("some_2018-01-12_05-07-59.tar.gz"))
-        when (result) {
-            is RetentionInfo.Info -> assertThat(result)
-                    .matches { it.isDaily && !it.isExpired }
-        }
+        assertThatRetentionInfo(result)
+                .matches { it.isDaily && !it.isExpired }
     }
 
     @Test
-    fun testExpireEighthsDaily() {
+    fun testExpireEighthDaily() {
         val result = retentionLogic.calculateRetentionInfo(File("some_2018-01-11_05-07-59.tar.gz"))
-        when (result) {
-            is RetentionInfo.Info -> assertThat(result)
-                    .matches { it.isDaily && it.isExpired }
-        }
+        assertThatRetentionInfo(result)
+                .matches { it.isDaily && it.isExpired }
     }
 
+    @Test
+    fun testKeepCurrentWeekly() {
+        val result = retentionLogic.calculateRetentionInfo(File("some_2018-01-14_05-07-59.tar.gz"))
+        assertThatRetentionInfo(result)
+                .matches { it.isWeekly && !it.isExpired }
+    }
+
+    @Test
+    fun testKeepSevenWeeklies() {
+        val result = retentionLogic.calculateRetentionInfo(File("some_2017-11-26_05-07-59.tar.gz"))
+        assertThatRetentionInfo(result)
+                .matches { it.isWeekly && !it.isExpired }
+    }
+
+    @Test
+    fun testExpireNinthWeekly() {
+        val result = retentionLogic.calculateRetentionInfo(File("some_2017-11-19_05-07-59.tar.gz"))
+        assertThatRetentionInfo(result)
+                .matches { it.isWeekly && it.isExpired }
+    }
+
+    @Test
+    fun testKeepCurrentMonthly() {
+        val result = retentionLogic.calculateRetentionInfo(File("some_2018-01-01_05-07-59.tar.gz"))
+        assertThatRetentionInfo(result)
+                .matches { it.isMonthly && !it.isExpired }
+    }
+
+    @Test
+    fun testKeepThirtySevenMonthlies() {
+        val result = retentionLogic.calculateRetentionInfo(File("some_2015-02-01_05-07-59.tar.gz"))
+        assertThatRetentionInfo(result)
+                .matches { it.isMonthly && !it.isExpired }
+    }
+
+    @Test
+    fun testExpireThirtySeventhMonthly() {
+        val result = retentionLogic.calculateRetentionInfo(File("some_2015-01-19_05-07-59.tar.gz"))
+        assertThatRetentionInfo(result)
+                .matches { it.isMonthly && it.isExpired }
+    }
+
+    @Test
+    fun testKeepAllTrueRetentionFlags() {
+        // first day of month and sunday
+        val result = retentionLogic.calculateRetentionInfo(File("some_2017-10-01_05-07-59.tar.gz"))
+        assertThatRetentionInfo(result)
+                .matches { it.isDaily && it.isWeekly && it.isMonthly && !it.isExpired }
+    }
+
+    @Test
+    fun testExpireFarFutureWithAllTrueRetentionFlags() {
+        // first day of month and sunday but far in the past thus expired
+        val result = retentionLogic.calculateRetentionInfo(File("some_2009-11-01_05-07-59.tar.gz"))
+        assertThatRetentionInfo(result)
+                .matches { it.isDaily && it.isWeekly && it.isMonthly && it.isExpired }
+    }
+
+    @Test
+    fun testExpireNearFuture() {
+        // first day of month and sunday but far in the past thus expired
+        val result = retentionLogic.calculateRetentionInfo(File("some_2017-12-28_05-07-59.tar.gz"))
+        assertThatRetentionInfo(result)
+                .matches { it.isDaily && !it.isWeekly && !it.isMonthly && it.isExpired }
+    }
+
+    private fun assertThatRetentionInfo(result: RetentionInfo): AbstractObjectAssert<*, RetentionInfo.Info> =
+            when (result) {
+                is RetentionInfo.Info -> assertThat(result)
+                is RetentionInfo.Error -> throw result.exception
+            }
 }
